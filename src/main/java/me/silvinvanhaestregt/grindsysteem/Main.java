c80a0b9dc9c6cf41ab0ef2d800498c253eeb40af
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-05-01 17:56:55 +0200

    Removed implements Listener from Main.java.

--- src/main/java/me/silvinvanhaestregt/grindsysteem/Main.java
+++ src/main/java/me/silvinvanhaestregt/grindsysteem/Main.java
@@ -1,18 +1,19 @@
 package me.silvinvanhaestregt.grindsysteem;
 
+import me.silvinvanhaestregt.grindsysteem.Currency.Grindcoins;
+import me.silvinvanhaestregt.grindsysteem.Currency.Grindxp;
 import me.silvinvanhaestregt.grindsysteem.Farms.Block;
 import me.silvinvanhaestregt.grindsysteem.Mines.MinesBlocks;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
     @Override
     public void onEnable(){
 
         // Add commands
-        this.getCommand("grindcoins").setExecutor(new Commands());
-        this.getCommand("grindxp").setExecutor(new Commands());
-        this.getCommand("testmessage").setExecutor(new Commands());
+        this.getCommand("grindcoins").setExecutor(new Grindcoins());
+        this.getCommand("grindxp").setExecutor(new Grindxp());
         // Event listeners
         getServer().getPluginManager().registerEvents(new Block(this), this);
         getServer().getPluginManager().registerEvents(new MinesBlocks(this), this);
