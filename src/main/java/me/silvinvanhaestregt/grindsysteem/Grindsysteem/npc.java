0a5b116b645e33f53c40b7d13e37eaef2b07212d
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-05-01 21:25:10 +0200

    Update

--- src/main/java/me/silvinvanhaestregt/grindsysteem/Grindsysteem/npc.java
+++ src/main/java/me/silvinvanhaestregt/grindsysteem/Grindsysteem/npc.java
@@ -1,8 +1,5 @@
 package me.silvinvanhaestregt.grindsysteem.Grindsysteem;
 
-import dev.sergiferry.playernpc.api.NPC;
-import dev.sergiferry.playernpc.api.NPCLib;
-
 public class npc {
 
 }
--- src/main/java/me/silvinvanhaestregt/grindsysteem/Main.java
+++ src/main/java/me/silvinvanhaestregt/grindsysteem/Main.java
@@ -4,9 +4,26 @@ import me.silvinvanhaestregt.grindsysteem.Currency.Grindcoins;
 import me.silvinvanhaestregt.grindsysteem.Currency.Grindxp;
 import me.silvinvanhaestregt.grindsysteem.Farms.Block;
 import me.silvinvanhaestregt.grindsysteem.Mines.MinesBlocks;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Main extends JavaPlugin implements Listener {
+
+    public static Main instance;
+    private File userFolder;
+    private HashMap<UUID, YamlConfiguration> usersConfig = new HashMap<>();
+
+    public static Main getInstance(){
+        return Main.instance;
+    }
+
     @Override
     public void onEnable(){
 
@@ -18,10 +35,30 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new MinesBlocks(this), this);
 
         // Config
+
+        saveDefaultConfig();
+
+        // Folders
+        userFolder = new File(getDataFolder(), "users");
+        userFolder.mkdirs();
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable(){
 
     }
+
+    public void onPlayerJoin(PlayerJoinEvent event){
+        File userFile = new File(userFolder, event.getPlayer() + ".yml");
+        if(!userFile.exists()){
+            try{
+                userFile.createNewFile();
+            } catch (IOException ex){
+
+            }
+        }
+        usersConfig.put(event.getPlayer().getUniqueId(), YamlConfiguration.loadConfiguration(userFile));
+    }
+
 }
