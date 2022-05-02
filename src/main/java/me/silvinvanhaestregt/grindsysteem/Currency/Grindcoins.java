ca1d1a84faf2eeea667cace357e6053e35e359d7
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-05-02 22:48:12 +0200

    Eddited Grindcoins.java and Grindxp.java

--- src/main/java/me/silvinvanhaestregt/grindsysteem/Currency/Grindcoins.java
+++ src/main/java/me/silvinvanhaestregt/grindsysteem/Currency/Grindcoins.java
@@ -1,34 +1,15 @@
 package me.silvinvanhaestregt.grindsysteem.Currency;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+public class Grindcoins{
 
-public class Grindcoins implements CommandExecutor {
+    private Grindcoins grindCoins;
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-
-        if(sender instanceof Player){
-            Player player = (Player) sender;
-
-            if(cmd.getName().equalsIgnoreCase("grindcoins")){
-                if(args.length > 0){
-                    if(player.hasPermission("grindsysteem.grindcoins")){
-                        if(args[0].equalsIgnoreCase("set")){
-                            // Set the amount of grindcoins a player has
-                        } else if (args[0].equalsIgnoreCase("add")) {
-                            // Give a certain amount of grindcoins to a player
-                        } else if (args[0].equalsIgnoreCase("remove")) {
-                            // Remove a certain amount of grindcoins from a player
-                        }
-                    }
-                } else {
-                    // Show the amount of grindcoins the player has
-                }
-            }
-        }
-        return false;
+    public Grindcoins(Grindxp grindxp){
+        this.grindCoins = grindCoins;
     }
+
+    public Grindcoins getGrindCoins(){
+        return grindCoins;
+    }
+
 }
--- src/main/java/me/silvinvanhaestregt/grindsysteem/Currency/Grindxp.java
+++ src/main/java/me/silvinvanhaestregt/grindsysteem/Currency/Grindxp.java
@@ -1,33 +1,15 @@
 package me.silvinvanhaestregt.grindsysteem.Currency;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+public class Grindxp{
 
-public class Grindxp implements CommandExecutor {
+    private Grindxp grindXp;
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]){
-
-        if(sender instanceof Player){
-            Player player = (Player) sender;
-
-            if(cmd.getName().equalsIgnoreCase("grindxp")){
-                if(args.length > 0) {
-                    if (player.hasPermission("grindsysteem.grindxp")) {
-                        if(args[0].equalsIgnoreCase("set")){
-                            // Set the amount of grindxp a player has
-                        } else if (args[0].equalsIgnoreCase("add")) {
-                            // Give a certain amount of grindxp to a player
-                        } else if (args[0].equalsIgnoreCase("remove")) {
-                            // Remove a certain amount of grindxp from a player
-                        }
-                    }
-                } else {
-                    // Show the amount of grindxp a player has
-                }
-            }
-        }
-        return false;
+    public Grindxp(Grindxp grindxp){
+        this.grindXp = grindxp;
     }
+
+    public Grindxp getGrindXp(){
+        return grindXp;
+    }
+
 }
